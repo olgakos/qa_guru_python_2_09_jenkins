854d32fdb4db2baba5cc590faca2718b06946c22
Olga <ok2304@yandex.ru>
2022-10-27 13:41:15 +0300

    commit3

--- conftest.py
+++ conftest.py
@@ -5,7 +5,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selene import Browser, Config
 from dotenv import load_dotenv
-
 from utils import attach
 
 DEFAULT_BROWSER_VERSION = "100.0"
