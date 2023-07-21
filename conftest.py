e3836185dd25bdeee8685ca020e235b6ef7b10b7
Olga <49266934+olgakos@users.noreply.github.com>
2023-07-21 13:32:05 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -6,6 +6,8 @@ from selenium.webdriver.chrome.options import Options
 from selene import Browser, Config
 from dotenv import load_dotenv
 
+from selene.support.shared import browser #! my
+
 from utils import attach
 
 DEFAULT_BROWSER_VERSION = "100.0"
@@ -27,7 +29,7 @@ def load_env():
 def setup_browser(request):
     browser_version = request.config.getoption('--browser_version')
     browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
-    options = Options()
+    options = Options()    
     selenoid_capabilities = {
         "browserName": "chrome",
         "browserVersion": browser_version,
